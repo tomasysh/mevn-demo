b1efefc92ca87f9a8845ddcc45507a1e718caf24
Tomas <tomasysh@gmail.com>
2022-05-26 22:36:57 +0800

    [前端專案] 移除不需使用的元件

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
@@ -35,6 +34,10 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+body {
+  background-color: lightgray;
 }
 
 a,
