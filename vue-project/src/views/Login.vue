f10d12e97d5be95f108f6b4c0040c8ba59ce4670
tomasysh <tomasysh@gmail.com>
2022-05-26 22:13:05 +0800

    [前端專案] 製作 login, signup page

---
+++ vue-project/src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <main>
+    <h1>登入</h1>
+    <form action="/signup" method="POST" @submit.prevent="login">
+      <div>
+        <label for="email">Email</label>
+        <input 
+          type="email" 
+          id="email" 
+          name="email"
+          required
+          v-model="email">
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input 
+          type="password"
+          id="password"
+          name="password"
+          required
+          v-model="password">
+      </div>
+      <button type="submit">登入</button>
+    </form>
+  </main>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+defineProps({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+const login = () => {
+  const loginData = {
+    email: email.value,
+    password: password.value
+  };
+  console.log('login', login);
+  const uri = 'http://localhost:4000/login';
+  axios.post(uri, loginData)
+    .then((res) => {
+      alert('登入成功!')
+    })
+    .catch((err) => {
+      alert('登入失敗......')
+    });
+}
+
+</script>
---
+++ vue-project/src/views/Signup.vue
@@ -0,0 +1,73 @@
+<template>
+  <main>
+    <h1>註冊頁</h1>
+    <form action="/signup" method="POST" @submit.prevent="signupAccount">
+      <div>
+        <label for="displayName">Display Name</label>
+        <input 
+          type="text" 
+          id="displayName" 
+          name="displayName" 
+          required 
+          v-model="displayName">
+      </div>
+      <div>
+        <label for="email">Email</label>
+        <input 
+          type="email" 
+          id="email" 
+          name="email"
+          required
+          v-model="email">
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input 
+          type="password"
+          id="password"
+          name="password"
+          required
+          v-model="password">
+      </div>
+      <button type="submit">註冊</button>
+    </form>
+  </main>
+</template>
+
+<script setup>
+import axios from 'axios';
+
+defineProps({
+  displayName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+const signupAccount = () => {
+  const signupData = {
+    displayName: displayName.value,
+    email: email.value,
+    password: password.value
+  };
+  console.log('signupData', signupData);
+  const uri = 'http://localhost:4000/signup';
+  axios.post(uri, signupData)
+    .then((res) => {
+      console.log('1231231', res)
+      alert('註冊成功 :)')
+    })
+    .catch((err) => {
+      alert('註冊失敗!')
+    });
+}
+
+</script>
